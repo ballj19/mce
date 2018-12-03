cb59bb72b4a0a881343342c147ad7e3adb5330f9
ballj19 <ballj19@gmail.com>
2018-12-02 16:31:26 -0800

    adventure

---
+++ Adventure/Adventure.UnitTests/CharacterTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using Adventure;
+
+
+namespace Adventure.UnitTests
+{
+    [TestFixture]
+    public class CharacterTests
+    {
+        private Character character;
+
+        [SetUp]
+        public void SetUp()
+        {
+            character = new Warrior("Jake");
+        }
+
+        [Test]
+        [TestCase(50,50)]
+        [TestCase(20,80)]
+        public void Hit_DamageLessThanHealth_HealthLowered(int damage, int health_remaining)
+        {
+            character.Hit(damage);
+            
+            Assert.That(character.health, Is.EqualTo(health_remaining));
+        }
+
+        [Test]
+        [TestCase(20, 100)]
+        [TestCase(50, 100)]
+        public void Heal_HealedToGreaterThanMax_HealthIsMaxHP(int heal, int health)
+        {
+            character.health = 90;
+
+            character.Heal(heal);
+
+            Assert.That(character.health, Is.EqualTo(health));
+        }
+
+        [Test]
+        [TestCase(20, 70)]
+        [TestCase(5, 55)]
+        public void Heal_HealedToLessThanMax_HealthIsMaxHP(int heal, int health)
+        {
+            character.health = 50;
+
+            character.Heal(heal);
+
+            Assert.That(character.health, Is.EqualTo(health));
+        }
+
+        [Test]
+        public void AddTitle_WhenCalled_TitleAddedToEndOfName()
+        {
+            string original_name = character.name;
+            character.AddTitle();
+
+            Assert.That(character.name, Does.StartWith(original_name));
+            Assert.That(character.name, Does.Not.EndsWith(original_name));
+        }
+    }
+}
---
+++ Adventure/Adventure/Character.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure
+{
+    public abstract class Character
+    {
+        public int max_health;
+        public int health;
+        public int energy;
+        public int mana;
+        public string name;
+        public int max_energy;
+        public int max_mana;
+
+        public EventHandler Dead;
+
+        public void Hit(int damage)
+        {
+            if(health - damage <= 0)
+            {
+                health = 0;
+                OnDeath();
+                //raise death event
+            }
+            else
+            {
+                health -= damage;
+            }
+
+            Console.WriteLine("Hit for {0} Damage, Health Remaining: {1}", damage, health);
+        }
+
+        public void Heal(int heal)
+        {
+            if(heal + health > max_health)
+            {
+                health = max_health;
+            }
+            else
+            {
+                health += heal;
+            }
+            Console.WriteLine("Healed for {0} Health, Health Remaining: {1}", heal, health);
+        }
+
+        protected virtual void OnDeath()
+        {
+            if (Dead != null)
+                Dead(this, EventArgs.Empty);
+        }
+
+        public void AddTitle()
+        {
+            List<string> titles = new List<string> { "The Defiled", "The Legendary", "The Beast" };
+
+            Random r = new Random();
+
+             name += " " + titles[r.Next(0, 2)];
+        }
+
+        public abstract bool ValidateWeapon(Weapon weapon);
+        
+    }
+}
